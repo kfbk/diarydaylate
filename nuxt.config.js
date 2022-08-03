47686a92391990a22e7967ec15f8e6450d3e7bf9
DESKTOP-DC91P5K\user <satou1497@gmail.com>
2022-08-03 11:16:11 +0900

    generate/routeを変えてみた

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,20 +85,92 @@ export default {
   build: {
   },
   generate: {
-    async routes() {
-      // ここのLimitは、本文のLimitと関係なく、１度作成すれば少なくてよい
-      const pages = await axios
-        .get('https://diarydaylate.microcms.io/api/v1/blogs?limit=100', {
-          headers: { 'X-MICROCMS-API-KEY': process.env.API_KEY }
+    // 「新人エンジニアの自習室」を参考に async を除いている
+    // async routes() {
+
+    routes() {
+      const blog = axios
+        .get("https://diarydaylate.microcms.io/api/v1/blogs", {
+          headers: { "X-MICROCMS-API-KEY": process.env.API_KEY }
         })
-        .then((res) =>
-          res.data.contents.map((content) => ({
-            route: `/${content.id}`,
-            payload: content
-          }))
-        )
-      console.log('satouroutes=', pages)
-      return pages
+        .then(res => {
+          return res.data.contents.map(content => {
+            // return "/blogs/" + blog.id;
+            return `/${content.id}`
+          });
+        });
+      return Promise.all([blog]).then(values => {
+        return values.join().split(",");
+      });
+
+      // // Promise.all を使ってみる（エラーになる）
+      // const axiosA = axios
+      //   .get('https://diarydaylate.microcms.io/api/v1/blogs?limit=100', {
+      //     headers: { 'X-MICROCMS-API-KEY': process.env.API_KEY }
+      //   })
+      // const result = await Promise.all([axiosA])
+      //   .then(([axiosA]) => {
+      //     return [
+      //       axiosA.data.contents.map((content) => ({
+      //         route: `/${content.id}`,
+      //         payload: content
+      //       }))
+      //     ]
+      //   })
+
+      // // Promise.all を使ってみる（エラーになる）
+      // const axiosA = axios
+      //   .get('https://diarydaylate.microcms.io/api/v1/blogs?limit=100', {
+      //     headers: { 'X-MICROCMS-API-KEY': process.env.API_KEY }
+      //   })
+      // const result = await Promise.all([axiosA])
+      //   .then(([axiosA]) => {
+      //     [
+      //       axiosA.data.contents.map((content) => ({
+      //         route: `/${content.id}`,
+      //         payload: content
+      //       }))
+      //     ]
+      //   })
+      // return result
+
+      // // ここのLimitは、本文のLimitと関係なく、１度作成すれば少なくてよい
+      // const pages = await axios
+      //   .get('https://diarydaylate.microcms.io/api/v1/blogs?limit=100', {
+      //     headers: { 'X-MICROCMS-API-KEY': process.env.API_KEY }
+      //   })
+      //   .then((res) =>
+      //     res.data.contents.map((content) => ({
+      //       route: `/${content.id}`,
+      //       payload: content
+      //     }))
+      //   )
+      // // console.log('satouroutes=', pages)
+      // return pages
+
+
+      // 次のデータが取れた
+      //　satouroutes= [                                                                                                17:37:26
+      //   {
+      //     route: '/u1hfhvny5jfr',
+      //     payload: {
+      //       id: 'u1hfhvny5jfr',
+      //       createdAt: '2022-08-01T23:01:27.932Z',
+      //       updatedAt: '2022-08-01T23:01:27.932Z',
+      //       publishedAt: '2022-08-01T23:01:27.932Z',
+      //       revisedAt: '2022-08-01T23:01:27.932Z',
+      //       title: '小説・パンク修理・買い物・歯医者',
+      //       body: '<p>昨日はいろいろあった。．．．
+      //       eyecatch: [Object],
+      //       category: [Object]
+      //     }
+      //   },
+      //   {...    
+      //     payload: {...
+      //       category: [Object]
+      //     }
+      //   }
+      // ]
     }
   }
 }
